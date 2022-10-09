7aa1d4ff0185519e98598756ead16c11e2ecdbb9
Yasenkova Sofia <yasenkovasofia@gmail.com>
2022-10-09 12:12:23 +0300

    lesson_03

---
+++ lesson_03/functions.py
@@ -0,0 +1,25 @@
+import json
+import sys
+
+from constants import MAX_LENGTH, ENCODING
+
+
+def get_message(client):
+    encoded_response = client.recv(MAX_LENGTH)
+    if isinstance(encoded_response, bytes):
+        try:
+            json_response = encoded_response.decode(ENCODING)
+            response = json.loads(json_response)
+            return response
+        except ValueError:
+            print('ValueError')
+
+
+
+def send_message(sock, message):
+    try:
+        js_message = json.dumps(message)
+        encoded_message = js_message.encode(ENCODING)
+        sock.send(encoded_message)
+    except TypeError:
+        print('TypeError')
