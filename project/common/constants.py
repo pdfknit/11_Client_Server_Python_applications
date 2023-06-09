2208af3816b9390bf67cf303f154e683c16eedf9
Yasenkova Sofia <yasenkovasofia@gmail.com>
2023-06-09 10:10:37 +0100

    lesson_12_db

--- project/common/constants.py
+++ project/common/constants.py
@@ -23,13 +23,22 @@ DESTINATION = 'to'
 MESSAGE = 'message'
 MESSAGE_TEXT = 'mess_text'
 EXIT = 'exit'
+GET_CONTACTS = 'get_contacts'
+LIST_INFO = 'data_list'
+REMOVE_CONTACT = 'remove'
+ADD_CONTACT = 'add'
+USERS_REQUEST = 'get_users'
 
 RESPONSE_200 = {RESPONSE: 200}
 # 400
 RESPONSE_400 = {
-            RESPONSE: 400,
-            ERROR: None
-        }
+    RESPONSE: 400,
+    ERROR: None
+}
 
+RESPONSE_202 = {
+    RESPONSE: 202,
+    LIST_INFO: None
+}
 # База данных для хранения данных сервера:
 SERVER_DATABASE = 'sqlite:///server_base.db3'
--- project/server.py
+++ project/server.py
@@ -1,5 +1,7 @@
 """Программа-сервер"""
 import argparse
+import configparser
+import os
 import socket
 import sys
 import logging
@@ -8,7 +10,8 @@ import threading
 import select
 
 from common.constants import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, RESPONSE, ERROR, DEFAULT_PORT, DESTINATION, \
-    SENDER, MESSAGE, MESSAGE_TEXT, EXIT, RESPONSE_400, RESPONSE_200
+    SENDER, MESSAGE, MESSAGE_TEXT, EXIT, RESPONSE_400, RESPONSE_200, DEFAULT_IP_ADDRESS, GET_CONTACTS, RESPONSE_202, \
+    LIST_INFO, ADD_CONTACT, REMOVE_CONTACT, USERS_REQUEST
 from common.metaclasses import ServerMaker, Port
 from common.utils import get_message, send_message
 from common.decos import Log
@@ -17,29 +20,33 @@ from server_database import ServerStorage
 sys.path.append('..')
 logger = logging.getLogger('server_log')
 
+# Флаг что был подключён новый пользователь
+new_connection = False
+conflag_lock = threading.Lock()
+
 
 @Log(logger)
-def arg_parser():
+def arg_parser(default_port=DEFAULT_PORT, default_address=DEFAULT_IP_ADDRESS):
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', default=DEFAULT_PORT, type=int, nargs='?')
-    parser.add_argument('-a', default='', nargs='?')
+    parser.add_argument('-p', default=default_port, type=int, nargs='?')
+    parser.add_argument('-a', default=default_address, nargs='?')
     namespace = parser.parse_args(sys.argv[1:])
     listen_address = namespace.a
     listen_port = namespace.p
     return listen_address, listen_port
 
 
-@Log(logger)
-def process_client_message(message):
-    if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
-            and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
-        logger.info(f'process_client_message - RESPONSE: 200')
-        return {RESPONSE: 200}
-    logger.info(f'process_client_message - RESPONSE: 400')
-    return {
-        RESPONSE: 400,
-        ERROR: 'Bad Request'
-    }
+# @Log(logger)
+# def process_client_message(message):
+#     if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
+#             and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
+#         logger.info(f'process_client_message - RESPONSE: 200')
+#         return {RESPONSE: 200}
+#     logger.info(f'process_client_message - RESPONSE: 400')
+#     return {
+#         RESPONSE: 400,
+#         ERROR: 'Bad Request'
+#     }
 
 
 def print_help():
@@ -94,7 +101,7 @@ class Server(threading.Thread, metaclass=ServerMaker):
                 if self.clients:
                     recv_data_lst, send_data_lst, err_lst = select.select(self.clients, self.clients, [], 0)
             except OSError:
-                pass
+                logger.error(f'OSError')
 
             if recv_data_lst:
                 for client_with_message in recv_data_lst:
@@ -108,7 +115,7 @@ class Server(threading.Thread, metaclass=ServerMaker):
                 try:
                     self.process_message(message, send_data_lst)
                 except:
-                    logger.info(f'Связь с клиентом с именем {message[DESTINATION]} была потеряна')
+                    logger.info(f'Связь с клиентом {message[DESTINATION]} потеряна')
                     self.clients.remove(self.names[message[DESTINATION]])
                     del self.names[message[DESTINATION]]
             self.messages.clear()
@@ -125,6 +132,7 @@ class Server(threading.Thread, metaclass=ServerMaker):
                 f'Пользователь {message[DESTINATION]} не зарегистрирован на сервере, отправка сообщения невозможна.')
 
     def process_client_message(self, message, client):
+        global new_connection
         logger.debug(f'Разбор сообщения от клиента : {message}')
 
         if ACTION in message and message[ACTION] == PRESENCE and TIME in message and USER in message:
@@ -148,8 +156,35 @@ class Server(threading.Thread, metaclass=ServerMaker):
             self.clients.remove(self.names[ACCOUNT_NAME])
             self.names[ACCOUNT_NAME].close()
             del self.names[ACCOUNT_NAME]
+            with conflag_lock:
+                new_connection = True
             return
 
+        # Запрос контакт-листа
+        elif ACTION in message and message[ACTION] == GET_CONTACTS and USER in message and \
+                self.names[message[USER]] == client:
+            response = RESPONSE_202
+            response[LIST_INFO] = self.database.get_contacts(message[USER])
+            send_message(client, response)
+
+        # Если это добавление контакта
+        elif ACTION in message and message[ACTION] == ADD_CONTACT and ACCOUNT_NAME in message and USER in message \
+                and self.names[message[USER]] == client:
+            self.database.add_contact(message[USER], message[ACCOUNT_NAME])
+            send_message(client, RESPONSE_200)
+
+        # Если это удаление контакта
+        elif ACTION in message and message[ACTION] == REMOVE_CONTACT and ACCOUNT_NAME in message and USER in message \
+                and self.names[message[USER]] == client:
+            self.database.remove_contact(message[USER], message[ACCOUNT_NAME])
+            send_message(client, RESPONSE_200)
+
+        # Если это запрос известных пользователей
+        elif ACTION in message and message[ACTION] == USERS_REQUEST and ACCOUNT_NAME in message \
+                and self.names[message[ACCOUNT_NAME]] == client:
+            response = RESPONSE_202
+            response[LIST_INFO] = [user[0] for user in self.database.users_list()]
+            send_message(client, response)
         else:
             response = 'RESPONSE 400'
             response[ERROR] = 'Запрос некорректен.'
